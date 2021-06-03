94318a0b93870bc67f516342174bdba34fd6daa5
Tom <thomas.spencer@codurance.com>
2021-06-03 09:52:43 +0100

    add code for Account test - still need to complete getAllByRole on cells in table

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
@@ -19,4 +19,20 @@ export default {
   setupFilesAfterEnv: [
     './tests/setup-tests.ts',
   ],
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.jest.json",
+    },
+    statements: 0,
+    branches: 0,
+    functions: 0,
+    lines: 0,
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/tests/e2e/"],
+  modulePathIgnorePatterns: ["<rootDir>/.*/__mocks__"],
+  moduleNameMapper: {
+    "~modules/(.*)": "<rootDir>/src/modules/$1",
+    "src/(.*)": "<rootDir>/src/$1",
+    "tests/(.*)": "<rootDir>/tests/$1",
+  },
 };
--- tests/acceptance/account.test.tsx
+++ tests/acceptance/account.test.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react';
-import {render, screen} from "@testing-library/react";
-import AccountPage from "../../src/pages";
+import {render, screen, within } from "@testing-library/react";
+import AccountPage from '../../src/pages';
+import userEvent from "@testing-library/user-event";
+it('works', async () => {
 
-it('works', () => {
+  // Date       || Amount || Balance
+  // 14/01/2012 || -500   || 2500
+  // 13/01/2012 || 2000   || 3000
+  // 10/01/2012 || 1000   || 1000
+
   render(<AccountPage />)
-  expect(screen.getByText('Hello Next.js')).toBeInTheDocument()
+
+  userEvent.type(screen.getByLabelText("Amount"), "1000");
+  userEvent.click(screen.getByRole('button', {name: /deposit/i}))
+  userEvent.type(screen.getByLabelText("Amount"), "2000");
+  userEvent.click(screen.getByRole('button', {name: /deposit/i}))
+  userEvent.type(screen.getByLabelText("Amount"), "500");
+  userEvent.click(screen.getByRole('button', {name: /withdraw/i}))
+
+  const rows = await screen.findAllByRole('row');
+  expect(within(rows[0]).getAllByRole('cell')[0]).toHaveTextContent("14/01/")
+  expect(screen.getByText('Hello Next.js 👋')).toBeInTheDocument()
 })

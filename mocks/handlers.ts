866bf997ba9debd29c21aaf217dffb344db32691
Tom <thomas.spencer@codurance.com>
2021-06-10 19:03:47 +0100

    Add handlers

---
+++ mocks/handlers.ts
@@ -0,0 +1,31 @@
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.post('/transactions', (req, res, ctx) => {
+    return res(
+      ctx.status(200);
+  }),
+  rest.get('/transactions', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        transactions: [
+          {
+            id: 3,
+            date: "14/01/2012",
+            amount: "-500"
+          },
+          {
+            id: 2,
+            date: "13/01/2012",
+            amount: "2000"
+          },
+          {
+            id: 1,
+            date: "10/01/2012",
+            amount: "1000"
+          }
+        ]
+      })
+    );
+  })
+]
